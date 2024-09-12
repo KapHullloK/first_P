35d35160fab0a9532614321538faae7cb2952e69
afro_0 <kirillov2136@gmail.com>
2024-09-12 13:29:39 +0300

    did hw_3

--- src/Main.java
+++ src/Main.java
@@ -3,69 +3,100 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
+        int x1 = (int) 1e6;
+        byte x2 = 12;
+        int x3 = 1000;
+        long x4 = (long) 1e12;
+        float y1 = 1e5F;
+        double y2 = 1e14;
 
-        var dog = 8.;
-        var cat = 3.6;
-        var paper = 763789;
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        System.out.println(x4);
+        System.out.println(y1);
+        System.out.println(y2);
+        System.out.println("--------------");
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
 
-        dog += 4;
-        cat += 4;
-        paper += 4;
+        float task2_1 = 27.12F;
+        long task2_2 = 987678965549L;
+        float task2_3 = 2.786F;
+        int task2_4 = 569;
+        int task2_5 = -159;
+        int task2_6 = 27897;
+        byte task2_7 = 67;
 
-        System.out.println("-------------");
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println(task2_1);
+        System.out.println(task2_2);
+        System.out.println(task2_3);
+        System.out.println(task2_4);
+        System.out.println(task2_5);
+        System.out.println(task2_6);
+        System.out.println(task2_7);
+        System.out.println("--------------");
 
-        dog *= 3.5;
-        cat *= 1.6;
-        paper *= 7639;
+        int teacher1 = 23;
+        int teacher2 = 27;
+        int teacher3 = 30;
+        int papers = 480;
+        int ans = papers / (teacher1 + teacher2 + teacher3);
+        System.out.format("На каждого ученика рассчитано %s листов бумаги\n", ans);
+        System.out.println("--------------");
 
-        System.out.println("-------------");
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        int boat = 16;
+        int time = 2;
+        int task1 = 20;
+        int task2 = 60 * 24;
+        int task3 = 60 * 24 * 3;
+        int task4 = 60 * 24 * 30;
 
-        System.out.println("-------------");
-        var friend = 19;
-        System.out.println(friend);
-        friend += 2;
-        System.out.println(friend);
-        friend /= 7;
-        System.out.println(friend);
+        System.out.format("За %s минут машина произвела %s штук бутылок\n", task1, task1 / time * boat);
+        System.out.format("За %s минут машина произвела %s штук бутылок\n", task2, task2 / time * boat);
+        System.out.format("За %s минут машина произвела %s штук бутылок\n", task3, task3 / time * boat);
+        System.out.format("За %s минут машина произвела %s штук бутылок\n", task4, task4 / time * boat);
+        System.out.println("--------------");
 
-        System.out.println("------------");
-        var frog = 3.5;
-        System.out.println(frog);
-        frog *= 10;
-        System.out.println(frog);
-        frog /= 3.5;
-        System.out.println(frog);
-        frog += 4;
-        System.out.println(frog);
+        int bottle = 120;
+        int white = 2;
+        int brown = 4;
 
-        System.out.println("------------");
-        var firstMan = 78.2;
-        var secondMan = 82.7;
-        System.out.println(firstMan + secondMan);
-        System.out.println(Math.abs(firstMan - secondMan));
+        System.out.format("В школе, где %s классов, нужно %s банок белой краски и %s банок коричневой краски\n",
+                bottle / (white + brown), bottle / (white + brown) * white, bottle / (white + brown) * brown);
+        System.out.println("--------------");
 
-        System.out.println("------------");
-        System.out.println(11 % 7);
 
-        System.out.println("------------");
-        var time = 640;
-        var timePerWorker = 8;
-        var amountWorkers = time / timePerWorker;
-        System.out.println("Всего работников в компании — " + amountWorkers + " человек");
+        int bananaAmount = 5;
+        int bananaWeight = 80;
+        int milkAmount = 200;
+        int milkWeight = 105;
+        int iceAmount = 2;
+        int iceWeight = 100;
+        int eggAmount = 4;
+        int eggWeight = 70;
+        float resG = bananaAmount * bananaWeight + milkAmount * milkWeight + iceAmount * iceWeight + eggAmount * eggWeight;
+        float resKg = resG / 1000F;
+        System.out.format("Gram - %s\n", resG);
+        System.out.format("Kg - %s\n", resKg);
+        System.out.println("--------------");
 
-        var newWorkers = amountWorkers + 94;
-        var newTime = newWorkers * timePerWorker;
-        System.out.println("Если в компании работает " + newWorkers + " человек, то всего " + newTime + " часов работы может быть поделено между сотрудниками");
+        int min = 250;
+        int max = 500;
+        int neededKg = 7;
+        int neededGrams = neededKg * 1000;
+        System.out.format("Если худеть на %s грамм - %s\n", min, neededGrams / min);
+        System.out.format("Если худеть на %s грамм - %s\n", max, neededGrams / max);
+        System.out.format("В среднем %s\n", ((neededGrams / min) + (neededGrams / max)) / 2);
+        System.out.println("--------------");
 
+        double mashaSalary = 67760;
+        double denisSalary = 83690;
+        double cristinaSalary = 76230;
+        System.out.format("Маша теперь получает %s рублей. Годовой доход вырос на %s рублей\n",
+                mashaSalary * 1.1, mashaSalary * 0.1 * 12);
+        System.out.format("Денис теперь получает %s рублей. Годовой доход вырос на %s рублей\n",
+                denisSalary * 1.1, denisSalary * 0.1 * 12);
+        System.out.format("Кристина теперь получает %s рублей. Годовой доход вырос на %s рублей\n",
+                cristinaSalary * 1.1, cristinaSalary * 0.1 * 12);
     }
 }
