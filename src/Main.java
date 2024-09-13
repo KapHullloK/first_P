705e0dda70a02f55137ea7635c1b450ab15dd855
afro_0 <kirillov2136@gmail.com>
2024-09-13 11:22:37 +0300

    did hw_5

--- src/Main.java
+++ src/Main.java
@@ -3,100 +3,97 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
+        boolean clientOS = 1 == 0;
 
-        int age = 12;
-        String ans = String.format("Если возраст человека равен %s, то ", age);
-
-        if (age < 18) {
-            ans += "он не достиг совершеннолетия, нужно немного подождать";
+        if (clientOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            ans += "он совершеннолетний";
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("---------------");
+
+
+        boolean clientOS_2 = 0 == 0;
+        int clientDeviceYear = 2015;
+
+        if (clientDeviceYear < 2015 && clientOS_2) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear < 2015 && !clientOS_2) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS_2) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("---------------");
+
+
+        int year = 2016;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year >= 1584) {
+            System.out.format("%s год является високосным\n", year);
+        } else {
+            System.out.format("%s год не является високосным\n", year);
+        }
+        System.out.println("---------------");
+
+
+        int deliveryDistance = 925;
+        String ans = "Потребуется дней: ";
+
+        if (deliveryDistance <= 20) {
+            ans += "1";
+        } else if (20 < deliveryDistance && deliveryDistance <= 60) {
+            ans += "2";
+        } else if (60 < deliveryDistance && deliveryDistance <= 100) {
+            ans += "3";
+        } else {
+            ans = "Доставки нет";
         }
 
         System.out.println(ans);
-        System.out.println("-------------");
+        System.out.println("---------------");
 
-        int degree = 6;
-        String ans2 = String.format("На улице %s градусов ", degree);
 
-        if (degree < 5) {
-            ans2 += "нужно надеть шапку";
-        } else {
-            ans2 += "можно идти без шапки";
-        }
+        int monthNumber = 12;
 
-        System.out.println(ans2);
-        System.out.println("-------------");
-
-        int speed = 333;
-        String ans3 = String.format("Если скорость %s, то ", speed);
-
-        if (speed <= 60) {
-            ans3 += "можно ездить спокойно";
-        } else {
-            ans3 += "придется заплатить штраф";
-        }
-
-        System.out.println(ans3);
-        System.out.println("-------------");
-
-        int teach_age = 21;
-        String ans4 = String.format("Если возраст человека равен %s, то ему нужно ходить ", teach_age);
-
-        if (2 <= teach_age && teach_age <= 6) {
-            ans4 += "в детский сад";
-        } else if (7 <= teach_age && teach_age <= 17) {
-            ans4 += "в школу";
-        } else if (18 <= teach_age && teach_age <= 24) {
-            ans4 += "в университете";
-        } else {
-            ans4 += "на работу";
-        }
-
-        System.out.println(ans4);
-        System.out.println("-------------");
-
-        int child_age = 7;
-        String ans5 = String.format("Если возраст ребенка равен %s, то ему ", child_age);
-
-        if (child_age <= 5) {
-            ans5 += "нельзя кататься на аттракционе";
-        } else if (5 < child_age && child_age < 14) {
-            ans5 += "можно кататься на аттракционе в сопровождении";
-        } else if (14 <= child_age) {
-            ans5 += "без сопровождения взрослого";
-        }
-
-        System.out.println(ans5);
-        System.out.println("-------------");
-
-        int capacity = 102;
-        int seat_place = 60;
-        int cur_situation = 92;
-
-        String ans6 = "";
-
-        if (0 <= cur_situation && cur_situation <= seat_place) {
-            ans6 += "Есть сидячие места";
-        } else if (seat_place < cur_situation && cur_situation <= capacity) {
-            ans6 += "Есть стоячие места";
-        } else {
-            ans6 += "Мест нет";
-        }
-
-        System.out.println(ans6);
-        System.out.println("-------------");
-        
-        int one = 1;
-        int two = 3;
-        int three = 7;
-
-        if (one >= two && one >= three) {
-            System.out.format("Самое большое число - %s", one);
-        } else if (two >= one && two >= three) {
-            System.out.format("Самое большое число - %s", two);
-        } else {
-            System.out.format("Самое большое число - %s", three);
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
         }
 
     }
