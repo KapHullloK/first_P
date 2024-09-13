b0067586ea676503c213e5b8af579686ebac4c94
afro_0 <kirillov2136@gmail.com>
2024-09-13 19:48:34 +0300

    fix hw_5

--- src/Main.java
+++ src/Main.java
@@ -40,14 +40,14 @@ public class Main {
         System.out.println(ans3);
         System.out.println("-------------");
 
-        int teach_age = 21;
-        String ans4 = String.format("Если возраст человека равен %s, то ему нужно ходить ", teach_age);
+        int teachAge = 21;
+        String ans4 = String.format("Если возраст человека равен %s, то ему нужно ходить ", teachAge);
 
-        if (2 <= teach_age && teach_age <= 6) {
+        if (2 <= teachAge && teachAge <= 6) {
             ans4 += "в детский сад";
-        } else if (7 <= teach_age && teach_age <= 17) {
+        } else if (7 <= teachAge && teachAge <= 17) {
             ans4 += "в школу";
-        } else if (18 <= teach_age && teach_age <= 24) {
+        } else if (18 <= teachAge && teachAge <= 24) {
             ans4 += "в университете";
         } else {
             ans4 += "на работу";
@@ -56,14 +56,14 @@ public class Main {
         System.out.println(ans4);
         System.out.println("-------------");
 
-        int child_age = 7;
-        String ans5 = String.format("Если возраст ребенка равен %s, то ему ", child_age);
+        int childAge = 7;
+        String ans5 = String.format("Если возраст ребенка равен %s, то ему ", childAge);
 
-        if (child_age <= 5) {
+        if (childAge <= 5) {
             ans5 += "нельзя кататься на аттракционе";
-        } else if (5 < child_age && child_age < 14) {
+        } else if (5 < childAge && childAge < 14) {
             ans5 += "можно кататься на аттракционе в сопровождении";
-        } else if (14 <= child_age) {
+        } else if (14 <= childAge) {
             ans5 += "без сопровождения взрослого";
         }
 
@@ -71,14 +71,14 @@ public class Main {
         System.out.println("-------------");
 
         int capacity = 102;
-        int seat_place = 60;
-        int cur_situation = 92;
+        int seatPlace = 60;
+        int curSituation = 92;
 
         String ans6 = "";
 
-        if (0 <= cur_situation && cur_situation <= seat_place) {
+        if (0 <= curSituation && curSituation <= seatPlace) {
             ans6 += "Есть сидячие места";
-        } else if (seat_place < cur_situation && cur_situation <= capacity) {
+        } else if (seatPlace < curSituation && curSituation <= capacity) {
             ans6 += "Есть стоячие места";
         } else {
             ans6 += "Мест нет";
