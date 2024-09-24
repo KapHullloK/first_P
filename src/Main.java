8085f652eeaf083bdcb1d1bb095ce437d4a4441c
afro_0 <kirillov2136@gmail.com>
2024-09-24 10:18:53 +0300

    did hw9

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,44 @@
-import java.lang.Math;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-
 public class Main {
 
     public static void main(String[] args) {
+        int[] lst1 = {2, 4, 5, 7, 22};
+        int ans1 = 0;
 
-        int[] lst1 = new int[3];
-
-        for (int i = 0; i < lst1.length; i++) {
-            lst1[i] = i + 1;
-            System.out.print(lst1[i]);
-            if (i + 1 != lst1.length) {
-                System.out.print(", ");
-            }
+        for (int j : lst1) {
+            ans1 += j;
         }
 
-        System.out.println();
-        float[] lst2 = {1.57F, 7.654F, 9.986F};
-        for (int i = 0; i < lst2.length; i++) {
-            System.out.print(lst2[i]);
-            if (i + 1 != lst2.length) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-        int[] lst3 = {1, 3, 6, 9};
-        for (int i = 0; i < lst3.length; i++) {
-            System.out.print(lst3[i]);
-            if (i + 1 != lst3.length) {
-                System.out.print(", ");
-            }
+        System.out.format("Сумма трат за месяц составила %s рублей\n", ans1);
+
+        int[] lst2 = {3, 6, 11, 213, 22};
+
+        int ans_max = -1;
+        int ans_min = (int) 1e6;
+
+        for (int j : lst2) {
+            if (ans_max < j) ans_max = j;
+            if (ans_min > j) ans_min = j;
         }
 
-        System.out.println();
-        for (int i = lst1.length - 1; i >= 0; i--) {
-            System.out.print(lst1[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
+        System.out.format("Минимальная сумма трат за неделю составила %s рублей." +
+                " Максимальная сумма трат за неделю составила %s рублей\n", ans_min, ans_max);
+
+
+        int[] lst3 = {123, 213, 11, 34243, 7897};
+
+        int ans3 = 0;
+
+        for (int j : lst3) {
+            ans3 += j;
         }
 
-        System.out.println();
-        for (int i = lst2.length - 1; i >= 0; i--) {
-            System.out.print(lst2[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
+        System.out.format("Средняя сумма трат за месяц составила %s рублей\n", (double) ans3 / lst3.length);
 
-        System.out.println();
-        for (int i = lst3.length - 1; i >= 0; i--) {
-            System.out.print(lst3[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
 
-        for (int i = 0; i < lst1.length; i++) {
-            if (lst1[i] % 2 != 0) {
-                lst1[i] += 1;
-            }
-        }
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
-        System.out.println();
-        for (int i = 0; i < lst1.length; i++) {
-            System.out.print(lst1[i]);
-            if (i != lst1.length - 1) {
-                System.out.print(", ");
-            }
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
 
     }
