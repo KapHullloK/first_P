e49f62ec8ea512a1538eb29985b8a31b8e5f1111
afro_0 <kirillov2136@gmail.com>
2024-09-24 20:35:21 +0300

    did hw10

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] lst1 = {2, 4, 5, 7, 22};
-        int ans1 = 0;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
 
-        for (int j : lst1) {
-            ans1 += j;
-        }
+        System.out.format("Ф. И. О. сотрудника — %s\n", fullName);
 
-        System.out.format("Сумма трат за месяц составила %s рублей\n", ans1);
+        System.out.format("Данные Ф. И. О. сотрудника для заполнения отчета — %s\n", fullName.toUpperCase());
 
-        int[] lst2 = {3, 6, 11, 213, 22};
+        fullName = "Иванов Семён Семёнович";
 
-        int ans_max = -1;
-        int ans_min = (int) 1e6;
-
-        for (int j : lst2) {
-            if (ans_max < j) ans_max = j;
-            if (ans_min > j) ans_min = j;
-        }
-
-        System.out.format("Минимальная сумма трат за неделю составила %s рублей." +
-                " Максимальная сумма трат за неделю составила %s рублей\n", ans_min, ans_max);
-
-
-        int[] lst3 = {123, 213, 11, 34243, 7897};
-
-        int ans3 = 0;
-
-        for (int j : lst3) {
-            ans3 += j;
-        }
-
-        System.out.format("Средняя сумма трат за месяц составила %s рублей\n", (double) ans3 / lst3.length);
-
-
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
-
+        System.out.format("Данные Ф. И. О. сотрудника —  %s", fullName.replace("ё", "е"));
     }
 }
