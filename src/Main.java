00bcbc4748c54a0cddac9cada9167eededc970c5
afro_0 <kirillov2136@gmail.com>
2024-09-26 18:13:38 +0300

    did hw11

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
+        int year = 2024;
+        int mob_year = 2022;
+        boolean os = false;
 
-        System.out.format("Ф. И. О. сотрудника — %s\n", fullName);
+        check_year(year);
+        check_mobile_os(os, mob_year);
+        get_days(34);
+    }
 
-        System.out.format("Данные Ф. И. О. сотрудника для заполнения отчета — %s\n", fullName.toUpperCase());
+    public static void check_year(int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.format("%s год — високосный год\n", year);
+        } else {
+            System.out.format("%s год — невисокосный год\n", year);
+        }
+    }
 
-        fullName = "Иванов Семён Семёнович";
+    public static void check_mobile_os(boolean mob_os, int date) {
+        int currentYear = LocalDate.now().getYear();
 
-        System.out.format("Данные Ф. И. О. сотрудника —  %s", fullName.replace("ё", "е"));
+        if (mob_os) {
+            if (currentYear > date)
+                System.out.println("Вам нужно скачать облегченную версию приложения под Android");
+            else {
+                System.out.println("Вам нужно скачать обычную версию приложения под Android");
+            }
+        } else {
+            if (currentYear > date)
+                System.out.println("Вам нужно скачать облегченную версию приложения под iOS");
+            else {
+                System.out.println("Вам нужно скачать облегченную версию приложения под iOS");
+            }
+        }
+    }
+
+    public static void get_days(int distance) {
+
+        String ans = "";
+
+        if (distance <= 20) {
+            ans += "1";
+        } else if (20 < distance && distance <= 60) {
+            ans += "2";
+        } else if (60 < distance && distance <= 100) {
+            ans += "3";
+        } else {
+            ans = "Доставки нет";
+        }
+
+        System.out.println(ans);
     }
 }
