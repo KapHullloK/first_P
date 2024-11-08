b4ad2aff8ef4126fa354a4e55dd98cf391222605
afro_0 <kirillov2136@gmail.com>
2024-11-08 18:49:59 +0300

    last fix hw16

--- src/Transport.java
+++ src/Transport.java
@@ -1,26 +1,27 @@
-public class Transport extends TransportABS {
+public abstract class Transport {
+    protected String modelName;
+    protected int wheelsCount;
 
-    @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
 
-    @Override
+
     public String getModelName() {
         return modelName;
     }
 
-    @Override
+
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
 
-    @Override
+
     public int getWheelsCount() {
         return wheelsCount;
     }
 
-    @Override
+
     public void setWheelsCount(int wheelsCount) {
         this.wheelsCount = wheelsCount;
     }
